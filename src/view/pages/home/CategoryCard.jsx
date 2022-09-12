57b0ce2e7f955dd5cc64fb68b63d1b1ba1b2ac16
nahidEvident <nahid@evidentbd.com>
2022-09-12 11:41:47 +0600

    category added

---
+++ src/view/pages/home/CategoryCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Card, Col } from "antd";
+import { useHistory } from "react-router-dom";
+
+export default function FeatureCard({ icon, title }) {
+  const history = useHistory();
+
+  return (
+    <Col lg={6} className="da-h-100" onClick={() => history.push(`/single-category/1`)}>
+      <Card className="hp-border-radius-xxl hp-dashboard-feature-card hp-cursor-pointer da-h-100">
+        <div
+          className="hp-d-flex-full-center hp-dashboard-feature-card-icon hp-border-radius-lg hp-bg-black-20 hp-bg-dark-80"
+          style={{ width: 48, height: 48 }}
+        >
+          {icon}
+        </div>
+
+        <div className="hp-d-flex hp-mt-12">
+          <span className="h5 hp-mb-0 hp-d-block hp-text-color-black-bg hp-text-color-dark-0 hp-font-weight-500 hp-mr-4">
+            {title}
+          </span>
+        </div>
+      </Card>
+    </Col>
+  );
+}
--- src/view/pages/home/index.jsx
+++ src/view/pages/home/index.jsx
@@ -1,10 +1,11 @@
 import { Col, Row } from "antd";
+import { BookSaved, BoxSearch, Gift, MoneySend } from "iconsax-react";
 import React from "react";
 import { useSelector } from "react-redux";
 import LandingFooter from "../landing/footer";
 import LandingHeader from "../landing/header";
+import CategoryCard from "./CategoryCard";
 import HeroSlider from "./HeroSlider";
-import Item from "./Item";
 export default function Category() {
   const products = useSelector((state) => state.ecommerce.products);
 
@@ -14,7 +15,7 @@ export default function Category() {
         <div className="hp-landing hp-bg-black-0 hp-bg-dark-90">
           <LandingHeader />
           <HeroSlider />
-          <section className="hp-landing-container hp-landing-hero hp-pt-16">
+          {/* <section className="hp-landing-container hp-landing-hero hp-pt-16">
             <div className="hp-px-sm-8 hp-px-16">
               <Row gutter={[24, 24]}>
                 {products.map((value) => (
@@ -22,6 +23,54 @@ export default function Category() {
                 ))}
               </Row>
             </div>
+          </section> */}
+
+          <section className="hp-landing-container hp-py-32">
+            <Row gutter={24}>
+              <CategoryCard
+                title="Airtime | Data | Bundles"
+                icon={
+                  <MoneySend
+                    size="24"
+                    variant="Bold"
+                    className="hp-text-color-black-bg hp-text-color-dark-0"
+                  />
+                }
+              />
+
+              <CategoryCard
+                title="Gift card"
+                icon={
+                  <Gift
+                    size="24"
+                    variant="Bold"
+                    className="hp-text-color-black-bg hp-text-color-dark-0"
+                  />
+                }
+              />
+
+              <CategoryCard
+                title="Utilities"
+                icon={
+                  <BoxSearch
+                    size="24"
+                    variant="Bold"
+                    className="hp-text-color-black-bg hp-text-color-dark-0"
+                  />
+                }
+              />
+
+              <CategoryCard
+                title="Education"
+                icon={
+                  <BookSaved
+                    size="24"
+                    variant="Bold"
+                    className="hp-text-color-black-bg hp-text-color-dark-0"
+                  />
+                }
+              />
+            </Row>
           </section>
           <LandingFooter />
         </div>
