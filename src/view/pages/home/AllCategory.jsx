1c014121d9cbf33d3f929c1098ef6a0d3ac905ba
nahidEvident <nahid@evidentbd.com>
2022-09-19 18:51:01 +0600

    comment removed

--- src/view/pages/home/AllCategory.jsx
+++ src/view/pages/home/AllCategory.jsx
@@ -1,20 +1,9 @@
 import { Row, Spin } from "antd";
 import React from "react";
 import useGetAllServices from "../../../hooks/rq/sohcitelCommunicator/useGetAllService";
-// import axiosInstance from "../../../configs/axios";
-import CategoryCard from "./CategoryCard";
 
 export default function AllCategory() {
-  // const [allCategories, setAllCategories] = useState([]);
-
   const { data: allCategories, isLoading } = useGetAllServices();
-
-  // useEffect(() => {
-  //   axiosInstance
-  //     .get("/get-all-service")
-  //     .then(({ data }) => setAllCategories(data?.data))
-  //     .catch((err) => console.log(err));
-  // }, []);
 
   return (
     <section className="hp-landing-container hp-py-32">
